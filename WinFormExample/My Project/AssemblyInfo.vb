305066c2eddf7e6b2d5b7d381a4405376e43d775
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-03-03 08:11:03 -0700

    Add project files.

---
+++ WinFormExample/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WinFormExample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("WinFormExample")>
+<Assembly: AssemblyCopyright("Copyright © HP 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d4d810eb-7083-4026-9dcb-fe4ca0c37fd9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
